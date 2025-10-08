d0e14d1942def33fcf791af9b7dc03bda5537ec7
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-08 17:16:42 +0300

    Moving the snake Forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """snake game"""
-from turtle import Turtle, Screen, turtles
+from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,45 +8,91 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-parts_list = []
+segments_list = []
+screen.tracer(0)
 
 for part in starting_positions:
-    snake_part = Turtle(shape="square")
-    snake_part.color("white")
-    snake_part.penup()
-    snake_part.goto(part) #to set the position of that square
-    parts_list.append(snake_part)
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(part) #to move it to that exact position x and y axis.
+    segments_list.append(new_segment)
     
-    
-
-def move():
-    for step in range(4):
-        parts_list[2].forward(60)
-        parts_list[1].forward(60)
-        parts_list[0].forward(60)
-
-       
-        
-move()
+   
+game_is_on = True 
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments_list:
+        seg.forward(20)
 
 
 
 
+# screen.update()
+# time.sleep(0.3) 
 
 
 
+# segments_list[2].forward(20)
+# segments_list[1].forward(20)
+# segments_list[0].left(90)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
+# segments_list[2].forward(20)
+# segments_list[1].left(90)
+# segments_list[1].forward(20)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
 
 
 
+# segments_list[2].left(90)
+# segments_list[2].forward(20)
+# segments_list[1].forward(20)
+# segments_list[0].left(90)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(2)
+# segments_list[2].forward(20)
+# segments_list[1].left(90)
+# segments_list[1].forward(20)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
 
 
 
+# segments_list[2].left(90)
+# segments_list[2].forward(20)
+# segments_list[1].forward(20)
+# segments_list[0].left(90)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
+# segments_list[2].forward(20)
+# segments_list[1].left(90)
+# segments_list[1].forward(20)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
 
 
-
-
-
-
+# segments_list[2].left(90)
+# segments_list[2].forward(20)
+# segments_list[1].forward(20)
+# segments_list[0].left(90)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
+# segments_list[2].forward(20)
+# segments_list[1].left(90)
+# segments_list[1].forward(20)
+# segments_list[0].forward(20)
+# screen.update()
+# time.sleep(0.3)
 
 
 
