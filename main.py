b08274fbc1d0fa606775d81b8dba1e88910d9f0c
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-08 23:40:40 +0300

    completing the movement functionality of the snake

--- main.py
+++ main.py
@@ -18,81 +18,35 @@ for part in starting_positions:
     new_segment.goto(part) #to move it to that exact position x and y axis.
     segments_list.append(new_segment)
     
-   
+    
+    
+def turn_left():
+    segments_list[0].left(90)
+def turn_right():
+    segments_list[0].right(90)    
+    
+    
+screen.listen()
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+
+
 game_is_on = True 
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-    for seg in segments_list:
-        seg.forward(20)
+    time.sleep(0.05)
+    
+    for l_two_segment in range(len(segments_list)-1, 0, -1):
+        segments_list[l_two_segment].goto(segments_list[l_two_segment-1].pos())
+           
+    segments_list[0].forward(20)
+        
+
+   
 
 
 
 
-# screen.update()
-# time.sleep(0.3) 
-
-
-
-# segments_list[2].forward(20)
-# segments_list[1].forward(20)
-# segments_list[0].left(90)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-# segments_list[2].forward(20)
-# segments_list[1].left(90)
-# segments_list[1].forward(20)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-
-
-
-# segments_list[2].left(90)
-# segments_list[2].forward(20)
-# segments_list[1].forward(20)
-# segments_list[0].left(90)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(2)
-# segments_list[2].forward(20)
-# segments_list[1].left(90)
-# segments_list[1].forward(20)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-
-
-
-# segments_list[2].left(90)
-# segments_list[2].forward(20)
-# segments_list[1].forward(20)
-# segments_list[0].left(90)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-# segments_list[2].forward(20)
-# segments_list[1].left(90)
-# segments_list[1].forward(20)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-
-
-# segments_list[2].left(90)
-# segments_list[2].forward(20)
-# segments_list[1].forward(20)
-# segments_list[0].left(90)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
-# segments_list[2].forward(20)
-# segments_list[1].left(90)
-# segments_list[1].forward(20)
-# segments_list[0].forward(20)
-# screen.update()
-# time.sleep(0.3)
 
 
 
